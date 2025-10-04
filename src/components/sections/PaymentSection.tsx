bdeec6cd1b70087cb1821634ed47cb0f649fee50
Godbeck <odamegodbeck@gmail.com>
2025-10-04 16:52:11 +0000

    style(PaymentSection): increase lock icon size for better visibility

--- src/components/sections/PaymentSection.tsx
+++ src/components/sections/PaymentSection.tsx
@@ -11,8 +11,8 @@ const PaymentSection = () => {
             <Image 
               src="/assets/padlock.svg" 
               alt="Lock icon" 
-              width={16} 
-              height={16}
+              width={20} 
+              height={20}
             />
             <span className="text-black-600 font-medium text-[13px] lg:text-[18px]">
               Secured by <span className="font-bold">Paystack</span>
