cfba27dd21ad02b86c56df86fc6c27dadd3cd5b1
Godbeck <odamegodbeck@gmail.com>
2025-10-04 17:49:54 +0000

    style(PaymentSection): adjust font size for payment security text

--- src/components/sections/PaymentSection.tsx
+++ src/components/sections/PaymentSection.tsx
@@ -14,7 +14,7 @@ const PaymentSection = () => {
               width={20} 
               height={20}
             />
-            <span className="text-black-600 font-medium text-[13px] lg:text-[18px]">
+            <span className="text-black-600 font-medium text-[12px] lg:text-[18px]">
               Secured by <span className="font-bold">Paystack</span>
             </span>
           </div>
