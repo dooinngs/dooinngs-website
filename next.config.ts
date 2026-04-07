19e7aae7c51a86e707cf0fe430d0d234aa064fca
Godbeck <odamegodbeck@gmail.com>
2026-04-07 09:49:22 +0000

    feat: add support for app linking by including apple-app-site-association and assetlinks.json files

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  headers: async () => [
+    {
+      source: "/.well-known/apple-app-site-association",
+      headers: [{ key: "Content-Type", value: "application/json" }],
+    },
+  ],
   images: {
     remotePatterns: [
       {
